dcf194b483ad772a7300ef642371cec86b89a6db
Guanjino <jjpereiranaranjo@gmail.com>
2025-03-02 19:25:00 +0100

    challenge terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import './App.css'
+import { useState } from "react";
 
 function App() {
- //Aquí tu código
-}
+    const [name, setName] = useState('Sofía')
+    const [newName, setNewName] = useState('')
 
+    function changeName(event) {
+        event.preventDefault()
+        if (newName !== '') {
+            setName(newName)
+            setNewName('')
+        }
+    }
+    return (
+        <div>
+            <h2>Teacher name: {name}</h2>
+            <ul>
+                <li onClick={() => setName('Bea')}>Bea</li>
+                <li onClick={() => setName('Aldo')}>Aldo</li>
+                <li onClick={() => setName('Yolanda')}>Yolanda</li>
+            </ul>
+            <form onSubmit={changeName}>
+                <input 
+                type="text" 
+                value={newName} 
+                onChange={(event) => setNewName(event.target.value) }
+                placeholder="add a name" 
+                />
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    )
+}
 export default App
